ee0b24248e13a0fa579abb1aa75167d636524df8
ZARTRT <438305438@qq.com>
2022-08-02 22:03:26 +0800

    chore: 格式化

--- Example/client.js
+++ Example/client.js
@@ -1,17 +1,17 @@
-const net = require('net');
+const net = require("net");
 const port = 3000;
-const host = '127.0.0.1';
+const host = "127.0.0.1";
 
-const client = net.createConnection(port,host)
+const client = net.createConnection(port, host);
 
-client.on('connect',()=>{
-    console.log('client is connected');
-})
+client.on("connect", () => {
+  console.log("client is connected");
+});
 
-client.on('data', (data) => {
-    console.log("client get server data: ", data.toString());
-  });
+client.on("data", (data) => {
+  console.log("client get server data: ", data.toString());
+});
 
-client.end('this is client');
+client.end("this is client");
 
-client.on('close', () => console.log('client is close'))
+client.on("close", () => console.log("client is close"));
